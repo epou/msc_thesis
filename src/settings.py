4e2b9af0e02773c82e08706d31fdef46dcb5cba9
Enric Pou <enricpou@gmail.com>
2020-12-23 11:43:14 +0100

    Dynamic root_dir

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,9 @@
 from configparser import ConfigParser, ExtendedInterpolation
+import os
+
+CONFIG_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../settings.ini')
 
 config = ConfigParser(interpolation=ExtendedInterpolation())
-config.read('../settings.ini')
+config.read(CONFIG_FILE)
+
+config.set("Common", "root_dir", os.path.realpath(os.path.dirname(CONFIG_FILE)))
